8f097494d5a2385ebd455aa3ac78ba38101c3f56
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-15 19:36:49 +0200

    Fix the text of the test

--- app/gameOfLife/createCell.test.js
+++ app/gameOfLife/createCell.test.js
@@ -1,9 +1,11 @@
-import createTableCells from "./createTable";
+import createTableCells from "./createCell";
 
-describe("createTableCells", () => {
-  test("generates the correct number of rows and columns", () => {
-    const cells = createTableCells(2, 2);
-    expect(cells.length).toBe(3);
-    expect(cells[0].length).toBe(3);
+describe("Given the function createTableCells", () => {
+  describe("When it receives the value of 2 rows and 2 columns", () => {
+    test("Then it should return a bidimensional array of 3 rows and 3 columns", () => {
+      const cells = createTableCells(2, 2);
+      expect(cells.length).toBe(3);
+      expect(cells[0].length).toBe(3);
+    });
   });
 });
